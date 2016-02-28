779f5b5f583d75307c5a36aa4b0e35c6184127e4
Jacob Hyllengren Larsson <derhyllengren@hotmail.com>
2016-02-27 16:40:39 -0800

    listar medlemmar och en idé om att redigera medlemsinfo

---
+++ Team_1_Halslaget_GK/medlem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team_1_Halslaget_GK
+{
+    public class medlem
+    {
+        public int ID { get; set;}
+        public string fornamn { get; set; }
+        public string efternamn { get; set; }
+        public double handikapp { get; set; }      
+    
+        public override string ToString()
+        {
+            return ID + " " + fornamn + " " + efternamn + " " + handikapp;
+        }
+    }
+}
