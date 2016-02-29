017c54feae4613cf6ae8c59ece10ddbc5696ea80
Myrtass <magy1405@student.miun.se>
2016-02-29 16:35:12 +0100

    lagt till ny sida

---
+++ Team_1_Halslaget_GK/PublishNewsAdmin.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Team_1_Halslaget_GK
+{
+    public partial class PublishNewsAdmin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
