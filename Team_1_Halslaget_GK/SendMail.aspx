8ff7655abe0a2b583b909bb76d4ed1aa650486df
erce1300 <erce1300@student.miun.se>
2016-03-08 23:24:01 +0100

    SendMail

---
+++ Team_1_Halslaget_GK/SendMail.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" ValidateRequest="false" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="SendMail.aspx.cs" Inherits="Team_1_Halslaget_GK.SendMail"%>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
+    <style type="text/css">
+        .auto-style1 {
+            font-size: small;
+            font-style: italic;
+        }
+        .auto-style2 {
+            font-size: x-large;
+        }
+    </style>
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="fullBox page-SendEmail">
+        <h1 class="auto-style2">Skicka mail:</h1>
+        <p class="auto-style1">Här kan personalen skicka mail till medlemmar i golfklubben.</p>
+        <p class="auto-style1">(Skriv in textmeddelandet i textboxen och tryck &quot;Skicka mail&quot; så skickas textmeddelandet till alla medlemmar i golfklubben)</p>
+        <br />
+    </div>  
+      
+    <br />
+<asp:Label ID="Label3" runat="server" Text="Välj vilken kategori som ni ämnar skicka till :"></asp:Label>
+<asp:DropDownList ID="DropDownList1" runat="server">
+    <asp:ListItem>Alla medlemmar</asp:ListItem>
+    <asp:ListItem>Senior</asp:ListItem>
+    <asp:ListItem>Student</asp:ListItem>
+    <asp:ListItem>Junior 0 - 12 år</asp:ListItem>
+    <asp:ListItem>Junior 13 - 21 år</asp:ListItem>
+</asp:DropDownList>
+<br />
+      
+    <br />
+      
+       <asp:Label ID="Label1" runat="server" Text="Ärende:"></asp:Label>
+    <br />
+    <asp:TextBox ID="TextBox2" runat="server" Height="23px" Width="218px"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Mailtext:"></asp:Label>
+    <br />
+       <asp:TextBox ID="TextBox1" runat="server" Height="158px" Width="1194px"></asp:TextBox>
+       <asp:Button ID="Button4" runat="server" Text="Skicka mail" CssClass="my-button" OnClick="Button4_Click" />
+    </asp:Content>
---
+++ Team_1_Halslaget_GK/SendMail.aspx.cs
@@ -0,0 +1,135 @@
+﻿using Npgsql;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Team_1_Halslaget_GK
+{
+    public partial class SendMail : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (DropDownList1.SelectedValue == "Alla medlemmar")
+            {
+                try
+                {
+
+                    MailMessage mailMessage = new MailMessage();
+                    MailAddress fromAddress = new MailAddress("Halslaget_GK@golf.se");
+                    mailMessage.From = fromAddress;
+                    mailMessage.To.Add("Alla_Medlemmar@golf.se");
+                    mailMessage.Body = TextBox1.Text + " Med Vanliga Halsningnar Halslaget GK";
+                    mailMessage.IsBodyHtml = true;
+                    mailMessage.Subject = "Meddelande till alla medlemmar: " + TextBox2.Text;
+                    SmtpClient smtpClient = new SmtpClient();
+                    smtpClient.Host = "localhost";
+                    smtpClient.Send(mailMessage);
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            if (DropDownList1.SelectedValue == "Senior")
+            {
+                try
+                {
+
+                    MailMessage mailMessage = new MailMessage();
+                    MailAddress fromAddress = new MailAddress("Halslaget_GK@golf.se");
+                    mailMessage.From = fromAddress;
+                    mailMessage.To.Add("senoir@golf.se");
+                    mailMessage.Body = TextBox1.Text + " Med Vanliga Halsningnar Halslaget GK";
+                    mailMessage.IsBodyHtml = true;
+                    mailMessage.Subject = "Meddelande till Seniorer: " + TextBox2.Text;
+                    SmtpClient smtpClient = new SmtpClient();
+                    smtpClient.Host = "localhost";
+                    smtpClient.Send(mailMessage);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (DropDownList1.SelectedValue == "Student")
+            {
+                try
+                {
+
+                    MailMessage mailMessage = new MailMessage();
+                    MailAddress fromAddress = new MailAddress("Halslaget_GK@golf.se");
+                    mailMessage.From = fromAddress;
+                    mailMessage.To.Add("student@golf.se");
+                    mailMessage.Body = TextBox1.Text + " Med Vanliga Halsningnar Halslaget GK";
+                    mailMessage.IsBodyHtml = true;
+                    mailMessage.Subject = "Meddelande till Studenter: " + TextBox2.Text;
+                    SmtpClient smtpClient = new SmtpClient();
+                    smtpClient.Host = "localhost";
+                    smtpClient.Send(mailMessage);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (DropDownList1.SelectedValue == "Junior 0 - 12 år")
+            {
+                try
+                {
+
+                    MailMessage mailMessage = new MailMessage();
+                    MailAddress fromAddress = new MailAddress("Halslaget_GK@golf.se");
+                    mailMessage.From = fromAddress;
+                    mailMessage.To.Add("junior0-12@golf.se");
+                    mailMessage.Body = TextBox1.Text + " Med Vanliga Halsningnar Halslaget GK";
+                    mailMessage.IsBodyHtml = true;
+                    mailMessage.Subject = "Meddelande till Junior 0 - 12 år: " + TextBox2.Text;
+                    SmtpClient smtpClient = new SmtpClient();
+                    smtpClient.Host = "localhost";
+                    smtpClient.Send(mailMessage);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (DropDownList1.SelectedValue == "Junior 13 - 21 år")
+            {
+                try
+                {
+
+                    MailMessage mailMessage = new MailMessage();
+                    MailAddress fromAddress = new MailAddress("Halslaget_GK@golf.se");
+                    mailMessage.From = fromAddress;
+                    mailMessage.To.Add("junior12-21@golf.se");
+                    mailMessage.Body = TextBox1.Text + " Med Vanliga Halsningnar Halslaget GK";
+                    mailMessage.IsBodyHtml = true;
+                    mailMessage.Subject = "Meddelande till Junior 13 - 21 år: " + TextBox2.Text;
+                    SmtpClient smtpClient = new SmtpClient();
+                    smtpClient.Host = "localhost";
+                    smtpClient.Send(mailMessage);
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+
+
+        }
+
+
+    }
+
+}
