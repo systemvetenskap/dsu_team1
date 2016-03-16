20c7ba08cc32421536ac46d0ec0ddec497ab7c84
Martin <myrtass@gmail.com>
2016-03-16 12:47:01 +0100

    buggar fixade

--- Team_1_Halslaget_GK/StartList.aspx.cs
+++ Team_1_Halslaget_GK/StartList.aspx.cs
@@ -194,13 +194,8 @@ namespace Team_1_Halslaget_GK
                 DataTable dt1 = getSingelCompPlaylist(gvComps.SelectedValue.ToString());
                 GridView1.DataSource = dt1;
 
-                btnGeneratePlaylist.Visible = true;
-
-                if(dt1.Rows.Count > 0)
-                {
-                    BtnGeneratePlaylistLag.Visible = false;
-                    btnGeneratePlaylist.Visible = false;
-                }            
+                BtnGeneratePlaylistLag.Visible = false;
+                btnGeneratePlaylist.Visible = true;           
 
                 GridView1.Columns[3].Visible = false;
 
@@ -212,12 +207,8 @@ namespace Team_1_Halslaget_GK
                 GridView1.DataSource = dt2;
 
                 BtnGeneratePlaylistLag.Visible = true;
+                btnGeneratePlaylist.Visible = false;
 
-                if (dt2.Rows.Count > 0)
-                {
-                    BtnGeneratePlaylistLag.Visible = false;
-                    btnGeneratePlaylist.Visible = false;
-                }
 
                 GridView1.Columns[3].Visible = true;
 
